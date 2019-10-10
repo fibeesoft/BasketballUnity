95ed2d2bd0c35b6438cbb385596460c36f9ec647
fibeesoft <fibeesoft@gmail.com>
2019-10-10 21:46:38 +0200

    GUI done, button 123 instantiate done

---
+++ Basketball Match Points Manager/Assets/Scripts/BtnPointClick.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BtnPointClick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GetButtonName()
+    {
+        //print(this.GetComponent<TMP_Text>().text);
+        print(this.name);
+    }
+}
---
+++ Basketball Match Points Manager/Assets/Scripts/CreateButtons.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CreateButtons : MonoBehaviour
+{
+    [SerializeField] GameObject btnPrefab;
+    [SerializeField] GameObject[] btnParentArray;
+
+    void Start()
+    {
+        CreateButtonsForSettingPoints(1);
+        CreateButtonsForSettingPoints(2);
+        
+    }
+
+    void CreateButtonsForSettingPoints(int teamNumber)
+    {
+        float posX = -200f;
+        float posY = -220f;
+
+        for(int i = 1; i < 4; i++)
+        {
+            GameObject btnPoint = Instantiate(btnPrefab);
+            btnPoint.transform.SetParent(btnParentArray[teamNumber - 1].GetComponent<Transform>());
+            btnPoint.transform.localScale = new Vector2(1, 1);
+            btnPoint.transform.localPosition = new Vector3(posX, posY, 1);
+            btnPoint.name = $"team{teamNumber}_{i}";
+            btnPoint.GetComponentInChildren<TMP_Text>().text = i.ToString();
+            posX += 200;
+        }
+    }
+}
