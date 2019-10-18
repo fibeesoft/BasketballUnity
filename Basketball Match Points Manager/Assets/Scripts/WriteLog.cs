4ccb7c966722e4973095819207d97e132c83f5c1
fibeesoft <fibeesoft@gmail.com>
2019-10-18 18:46:13 +0200

    streamwrite to the file - started

---
+++ Basketball Match Points Manager/Assets/Scripts/WriteLog.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class WriteLog : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private string fileName;
+    FileStream fs;
+    StreamWriter sw;
+    void Start()
+    {
+        //fileName = @"d://test.txt";
+        fileName = "test.txt";
+        fs = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+    }
+
+    public void WriteToFile(string message){
+        sw = new StreamWriter(fs);
+        fs.Seek(0, SeekOrigin.End);
+        DateTime dt = DateTime.Now;
+        sw.Write(dt.ToString("HH:mm:ss dd.MM.yy"));
+        sw.Write("     ");
+        sw.WriteLine(message);
+        sw.WriteLine();
+        sw.Flush();
+        //sw.Close();
+    }
+}
