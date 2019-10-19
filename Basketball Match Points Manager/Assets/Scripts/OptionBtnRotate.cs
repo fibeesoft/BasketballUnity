09493433ec7a4a7ebdb90f23b0f46a1394c38473
fibeesoft <fibeesoft@gmail.com>
2019-10-19 10:06:38 +0200

    animation cog wheel started

---
+++ Basketball Match Points Manager/Assets/Scripts/OptionBtnRotate.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionBtnRotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] Animation anim;
+    [SerializeField] Animator animator;
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animation>();
+        animator = gameObject.GetComponent<Animator>();
+        animator.SetBool("isRotating", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RotateTheCog()
+    {
+        animator.SetBool("isRotating", true);
+        if(animator.GetBool("isRotating") == true)
+        {
+            anim.Play("CogWheelAnim");
+            animator.SetBool("isRotating", false);
+        }
+        
+        print("rotate");
+    }
+}
